465ee42443758120d111594d601b73b242d4f116
Banana1600 <61030031@kmitl.ac.th>
2026-05-22 14:02:24 +0700

    เพิ่มหหน้า exam.html

--- script.js
+++ script.js
@@ -205,6 +205,27 @@ function closeAddCourse() {
 }
 
 
+let slides=document.querySelectorAll(".slide");
+let index=0;
+
+function showSlide(){
+
+slides.forEach(s=>s.classList.remove("active"));
+
+slides[index].classList.add("active");
+
+index++;
+
+if(index>=slides.length){
+index=0;
+}
+
+}
+
+setInterval(showSlide,3000);
+
+
+
 /* โหลดข้อมูลตอนเปิดหน้า */
 
 document.addEventListener("DOMContentLoaded", () => {
